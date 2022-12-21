f137c61d1a72e348a399ccc36516460f80db5e28
JohnHumphrey2099 <jhumphrey1516@gmail.com>
2022-12-21 12:53:07 -0600

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.humphrey.c195project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.humphrey.model to javafx.fxml;
+    exports com.humphrey.model;
+    exports com.humphrey.controller;
+    opens com.humphrey.controller to javafx.fxml;
+}
